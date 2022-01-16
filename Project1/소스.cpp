0eb0bc46ca98e7289c5c3650dd8c4346a86d314e
garshi <90501021+garshi@users.noreply.github.com>
2022-01-16 18:40:24 +0900

    FINAL

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,26 +1,25 @@
 #include <stdio.h>
-#include< stdlib.h>
+#include<stdlib.h>
 
-int main(void) {
+char WORD[1000000];											// 단어를 입력받을 배열
+int num = 0;												
 
+int main(void) {											// 메인함수
 
 	
-	char WORD[1000000];								// 단어 char
+	int A[26] = { 0 };										// 각 알파벳의 개수를 입력하기 위한 배열
+
+	fgets(WORD, sizeof(WORD), stdin);						// 단어 입력 받기
 	
-	int A[52] = { 0 };										// 각 알파벳의 출현 횟수
-
-	fgets(WORD, sizeof(WORD), stdin);				// 단어 입력 받기
-
-	int MAX = 0;							// 최대값
-	int MAX_num = 0;						// 최대값을 가진 알파벳의 순서
+	int MAX_num = 0;										// 최대값의 알파벳 개수 받기 위한 int
 
 
 
 
-	for (int i = 0; i < 1000000; i++) {			// 각 알파벳 개수 판별 반복문
+	for (int i = 0; i < 1000000; i++) {						// 각 알파벳 개수 판별 반복문
 
 
-		int num = 0;							// 현재 반복문에서 판정하고 있는 알파벳
+		num = 0;											// 현재 반복문에서 판정하고 있는 알파벳
 
 		if (WORD[i] == '\0') break;							// 문자열 끝에서 종료
 
@@ -31,16 +30,20 @@ int main(void) {
 			
 		}
 
+		num = 0;
+
 		for (char b = 'A'; b <= 'Z'; b++) {
-			if (WORD[i] == b) A[num+26] ++;			// 대문자 확인 반복문
+			if (WORD[i] == b) A[num] ++;			// 대문자 확인 반복문
 			num++;
 		}
 		
 	}
 
 
+	int MAX = A[0];							// 최대값
 
-	for (int i = 1; i < 52; i++) {			// 최대값 판정
+
+	for (int i = 1; i < 26; i++) {			// 최대값 판정
 
 		if (MAX < A[i]) {
 
@@ -49,10 +52,13 @@ int main(void) {
 			A[i] = 0;
 
 		}
+		
 	}
 
+	A[0] = 0;
 
-	for (int i = 0; i < 52; i++) {
+
+	for (int i = 0; i <26; i++) {
 
 		if (MAX == A[i]) {				// 최대값과 같은 값이 있다면 
 			printf("?");				// ? 출력후 
@@ -62,9 +68,8 @@ int main(void) {
 	}
 	
 
-	if (MAX < 26) printf("%c", 97 + MAX_num);			// MAX가 소문자 일시 출력
-
-	else if (MAX >= 26) printf("%c", 39 + MAX_num);		// MAX가 대문자 일시 출력 
+	
+	 printf("%c", 65 + MAX_num);		// 가장 많이 나온 알파벳 출력
 	
 
 }
